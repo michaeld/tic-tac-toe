e9e8de1d7650813d6645a61b479a781e421218b9
Michael Dumont <Michael.Dumont@blackboard.com>
2014-12-10 17:46:59 -0500

    update readme

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ var AWS = require('aws-sdk');
 AWS.config.region = 'us-east-1'
 var sqs = new AWS.SQS();
 
+var queueUrl = 'https://sqs.us-east-1.amazonaws.com/854757141155/tictactoe';
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // set routes
-app.get('/tictactoe/eventstream', function(req, res) {
+app.get('/tictactoe/recordplayermark', function(req, res) {
   
   var params = {
     MessageBody: JSON.stringify({ command : "RecordPlayerMark", data: req.query }), /* required */
-    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/854757141155/tictactoe', /* required */
+    QueueUrl: queueUrl, /* required */
     DelaySeconds: 0
   };
 
@@ -27,6 +29,21 @@ app.get('/tictactoe/eventstream', function(req, res) {
   res.send('received')
 });
 
+app.get('/tictactoe/recordoutcome', function(req, res) {
+  
+  var params = {
+    MessageBody: JSON.stringify({ command : "RecordOutcome", data: req.query }), /* required */
+    QueueUrl: queueUrl, /* required */
+    DelaySeconds: 0
+  };
+
+  sqs.sendMessage(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+
+  res.send('received')
+});
 
 // Set server port
 app.listen(4000);
